09405b130c7eb6305cef0190426afa231a0f403a
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-09-26 00:13:32 -0300

    create method fetch by filters

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,9 +3,22 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class UserRepository
 {
+
+    public function fetch(array $filters): LengthAwarePaginator
+    {
+        return User::query()
+            ->when(isset($filters['name']), function ($query) use ($filters) {
+                return $query->where('name', 'like', '%' . $filters['name'] . '%');
+            })
+            ->when(isset($filters['email']), function ($query) use ($filters) {
+                return $query->where('email', $filters['email']);
+            })
+            ->paginate($filters['perPage'] ?? 10);
+    }
     public function create(array $userData): User
     {
         return User::create($userData);
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\User;
 use App\Repositories\UserRepository;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Hash;
 
 class UserService
@@ -15,6 +16,10 @@ class UserService
         $this->userRepository = $userRepository;
     }
 
+    public function fetch(array $filters): LengthAwarePaginator
+    {
+        return $this->userRepository->fetch($filters);
+    }
     public function create(array $userData): User
     {
         $userData['password'] = Hash::make($userData['password']);
