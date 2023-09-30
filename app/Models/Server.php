172236da46695d54df892a2fee0b083365e130c0
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-09-30 01:10:12 -0300

    add relationship with server status in server

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 /**
  * @property string $name
  * @property int $id
@@ -18,4 +21,14 @@ class Server extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function status(): HasMany
+    {
+        return $this->hasMany(ServerStatus::class);
+    }
+
+    public function current_status(): HasOne
+    {
+        return $this->hasOne(ServerStatus::class)->latest();
+    }
 }
